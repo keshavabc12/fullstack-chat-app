f5c1447d132a30e843c0f6633942d00612bed266
keshav <keshavabc2003@gmail.com>
2025-07-02 20:34:09 +0530

    project completed

--- backend/src/index.js
+++ backend/src/index.js
@@ -12,7 +12,7 @@ dotenv.config();
 
 
 const PORT = process.env.PORT || 5000;
-const__dirname = path.resolve();
+
 
 // ✅ Must come BEFORE routes!
 app.use(cors({
@@ -31,11 +31,7 @@ app.use("/api/messages", messageRoutes);
 
 if(process.env.NODE_ENV === "production") {
 
-  app.use(express.static(path.join(__dirname, "../frontend/dist")));
-  app.get("*", (req, res) => {
-    res.sendFile(path.join(__dirname, "../frontend","dist", "index.html"));
-
-  })
+  
 
 }
 
